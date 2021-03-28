28b07e94a29434188deee85a5f0d952a0d6aa2d6
ohmpatel46 <ohmpatel46@gmail.com>
2021-03-28 21:52:12 +0530

    V 0.0.2
    Added Navbar Section.
    Made website responsive for mobile and desktop.

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './MainNavigation.css';
 import MainHeader from './MainHeader';
+import SideDrawer from './SideDrawer';
+import BackDrop from '../UIelements/Backdrop';
 import Navlinks from './Navlinks';
 import {Link} from 'react-router-dom';
 
 function MainNavigation(props)
 {
+    const [drawerIsOpen,setDrawerIsOpen]=useState(false);
+    function openDrawerHandler(){
+        setDrawerIsOpen(true);
+    };
+    function closeDrawerHandler(){
+        setDrawerIsOpen(false);
+    };
     return(
-        <MainHeader>
-            <button className="main-navigation__menu-btn">
-                <span/>
-                <span/>
-                <span/>
-            </button>
-            <h1 className="main-navigation__title">
-                <Link to="/">Your Places</Link>
-            </h1>
-            <nav className="main-navigation__header-nav">
-                <Navlinks/>
-            </nav>
-        </MainHeader>
+        <React.Fragment>
+            {drawerIsOpen &&(
+                <BackDrop onClick={closeDrawerHandler}/>
+            )}
+            
+                <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+                <nav className="main-navigation__drawer-nav">
+                    <Navlinks/>
+                </nav>
+                </SideDrawer>
+            
+            
+            
+            <MainHeader>
+                <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
+                    <span/>
+                    <span/>
+                    <span/>
+                </button>
+                <h1 className="main-navigation__title">
+                    <Link to="/">Your Places</Link>
+                </h1>
+                <nav className="main-navigation__header-nav">
+                    <Navlinks/>
+                </nav>
+            </MainHeader>
+        </React.Fragment>
     );
 }
 
--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {CSSTransition} from 'react-transition-group';
 import './SideDrawer.css';
 
 function SideDrawer(props)
 {
+    const content= (
+    <CSSTransition in={props.show} timeout={200} classNames="slide-in-left" mountOnEnter unmountOnExit><aside className="side-drawer" onClick={props.onClick}>{props.children}</aside></CSSTransition>
+    );
+    
     return(
-        <aside className="side-drawer">{props.children}</aside>
+        ReactDOM.createPortal(content,document.getElementById("drawer-hook"))
     );
 }
 
